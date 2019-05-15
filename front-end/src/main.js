3d05ca476be73802082e90a39ef5805f683bd394
AKD47 <van83horn@gmail.com>
2019-05-15 17:16:25 +0300

    start axios config

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 import BootstrapVue from 'bootstrap-vue'
 import ElementsUI from 'element-ui'
 import { Loading } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import VueDataTables from 'vue-data-tables'
+
+axios.defaults.baseURL = '';
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.interceptors.request.use( function (config) {
+    return config;
+});
+Vue.prototype.$http = axios;
 
 Vue.use(BootstrapVue)
 Vue.use(ElementsUI, Loading)
